41315a6ed3d6f26b927a0224823a1d05affb11a5
Sebastian Ariel <sebas.sm.sac@gmail.com>
2019-08-01 16:23:25 -0300

    se agrega ejercicio 3

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 var link = document.getElementById("link");
+var url = "https://www.google.com.ar";
 
 link.addEventListener("click", event => {
   event.stopImmediatePropagation();
   event.preventDefault();
 
-  var container = document.createElement("div");
-  container.id = "container";
-  //containerId = document.getElementById("container");
+  if(!document.getElementById("container")){
+    var container = document.createElement("div");
+    container.id = "container";
+    document.body.appendChild(container);
 
-  //var text = containerId.createElement("p");
-  //text.innerText = "Esta seguro que desea abandonar la pagina?";
+    var text = document.createElement("p");
+    text.innerText = "¿Esta seguro que desea abandonar la pagina?";
 
-  document.body.appendChild(container);
+    var btnAccept = document.createElement("button");
+    btnAccept.innerText = "Aceptar";
+    btnAccept.id = "accept";
+    var btnCancel = document.createElement("button");
+    btnCancel.innerText = "Cancelar";
+    btnCancel.id = "cancel";
+
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(text);
+    fragment.appendChild(btnAccept);
+    fragment.appendChild(btnCancel);
+    document.getElementById("container").appendChild(fragment);
+
+    document.getElementById("accept").addEventListener("click", event => {
+      event.stopImmediatePropagation();
+      event.preventDefault();
+      window.location.href = url;
+    });
+    document.getElementById("cancel").addEventListener("click", event => {
+      event.stopImmediatePropagation();
+      event.preventDefault();
+      document.getElementById("container").remove();
+    });
+  } 
 });
